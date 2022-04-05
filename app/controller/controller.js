6312eedca62dec027c8f1797174bbe2c9a6ef5cd
Sagar Kadam <40513747+kadamsagar039@users.noreply.github.com>
2022-04-05 09:37:38 +0530

    Update controller.js

--- app/controller/controller.js
+++ app/controller/controller.js
@@ -3,7 +3,7 @@ module.exports = {
 
         let response = {
             success: true,
-            message: "Hello World!!!"
+            message: "Hello World.................!!!"
         }
 
         res.status(200).send(response)
